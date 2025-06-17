99e5f10eb16e643936dcafa07d9236f675a8fa00
Nallely Soto <nallelysotoyocupicio@gmail.com>
2025-06-16 23:38:09 -0700

    holiwi

--- src/Portada.tsx
+++ src/Portada.tsx
@@ -1,6 +1,10 @@
 import boda from "./assets/boda.jpeg";
 function Portada(){
-    return <img src={boda} alt="" className="w-full max-w-md mx-auto object-cover object-center"/>
+    return <div>
+        <img src={boda} alt="" className="w-full max-w-md mx-auto object-cover object-center" />
+        <h1> Holiwis</h1>
+    </div>
+
 
 }
 export default Portada;
